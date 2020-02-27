d139d64d83dcd29d9991e88076db02c510f93950
VodeniZeko <salexy.es@gmail.com>
2020-02-27 00:51:58 -0800

    routes and form

---
+++ client/src/Movies/UpdateForm.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const initialItem = {
+  title: "",
+  director: "",
+  metascore: "",
+  stars: []
+};
+
+const UpdateForm = props => {
+  const [item, setItem] = useState(initialItem);
+  const { id } = useParams();
+
+  const changeHandler = e => {
+    setItem({
+      ...item,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:3333/items/${id}`, item)
+      .then(res => {
+        console.log(res);
+        props.setItems(res.data);
+        props.history.push(`/item-list/${id}`);
+      })
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div style={{ display: "table", margin: "0 auto" }}>
+      <h2>Update Item</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={changeHandler}
+          placeholder="title"
+          value={item.title}
+        />
+        <div className="baseline" />
+
+        <input
+          type="text"
+          name="director"
+          onChange={changeHandler}
+          placeholder="director"
+          value={item.director}
+        />
+        <div className="baseline" />
+
+        <input
+          type="text"
+          name="metascore"
+          onChange={changeHandler}
+          placeholder="metascore"
+          value={item.metascore}
+        />
+        <div className="baseline" />
+
+        <input
+          type="text"
+          name="stars"
+          onChange={changeHandler}
+          placeholder="stars"
+          value={item.stars}
+        />
+        <div className="baseline" />
+
+        <button className="md-button form-button">Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateForm;
